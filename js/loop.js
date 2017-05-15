69c2ace85ddefece1901d75131ae3096de2421ef
Mykhailo Tsymbala <mishatsumbala@gmail.com>
2017-05-15 11:36:01 +0000

    lastcommit

--- js/loop.js
+++ js/loop.js
@@ -1,6 +1,8 @@
 var n = Number(prompt("Enter number"));
 document.write('n fib ='+fib(n));
 function fib(n) {
+  if (n < 0)
+  return "Can't be less than 0";
   var a = 1,
       b = 1;
   for (var i = 3; i <= n; i++) {
--- js/recursion.js
+++ js/recursion.js
@@ -1,6 +1,8 @@
 var n = Number(prompt("Enter number"));
 document.write('n-ne fib ='+fib(n));
 function fib(n) {
-    if (n <= 2) return 1;
+    if (n < 0)
+    return "Can't be less than 0";
+    else if (n <= 2) return 1;
     return fib(n-1) + fib(n-2);
 }
